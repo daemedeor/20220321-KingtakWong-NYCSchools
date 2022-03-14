7ce135c97cba1f935981f46e25116b4e6d970281
Kingtak Justin Wong <justin@kingtak.us>
2022-03-14 13:29:14 -0700

    Initial Commit

---
+++ 20220321-KingtakWong-NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20220321-KingtakWong-NYCSchools
+//
+//  Created by Kingtak Justin Wong on 3/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
