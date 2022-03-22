4136911a718eff7b5c3c937d1d07f4d7f062f22b
Kingtak Justin Wong <justin@kingtak.us>
2022-03-21 21:52:19 -0700

    Changing themes with KVO

--- 20220321-KingtakWong-NYCSchools/ViewControllers/SchoolInformationController.swift
+++ 20220321-KingtakWong-NYCSchools/ViewControllers/SchoolInformationController.swift
@@ -12,12 +12,14 @@ import MapKit
 class SchoolInformationController: UIViewController {
 
     var school: School?
+    static let changeThemeKey = "theme.change.notificationKey"
 
     @IBOutlet weak var desciptionTextView: UITextView!
     @IBOutlet weak var phoneNumber: UILabel!
     @IBOutlet weak var email: UILabel!
     @IBOutlet weak var website: UILabel!
-
+    @IBOutlet weak var themeButton: UIBarButtonItem!
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
@@ -25,5 +27,27 @@ class SchoolInformationController: UIViewController {
         self.phoneNumber.text = school?.phoneNumber
         self.email.text = school?.email
         self.website.text = school?.website
+        NotificationCenter.default.addObserver(self, selector: #selector(changeThemePress), name: Notification.Name(rawValue: SchoolInformationController.changeThemeKey), object: nil)
+    }
+    
+    @objc func changeThemePress() {
+        var currentThemeColor: UIColor = .white
+        
+        switch traitCollection.userInterfaceStyle {
+        case .dark:
+            currentThemeColor = .blue
+        case .light:
+            currentThemeColor = .brown
+        case .unspecified:
+            currentThemeColor = .cyan
+        @unknown default:
+            currentThemeColor = .white
+        }
+        
+        self.view.backgroundColor = currentThemeColor
+    }
+    
+    @IBAction func pressTheme(_ sender: Any) {
+        NotificationCenter.default.post(name: Notification.Name(rawValue: SchoolInformationController.changeThemeKey), object: self)
     }
 }
